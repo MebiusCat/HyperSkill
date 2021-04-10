89ae991ae6e950399378525d2895a904b732c003
Snetkova Natalia <snetkova.natalia@gmail.com>
2021-04-10 07:39:48 +0300

    Stage 4/5

--- Food Blog Backend/blog.py
+++ Food Blog Backend/blog.py
@@ -42,8 +42,21 @@ class RecipeDatabase:
             meal_id INTEGER NOT NULL,
             FOREIGN KEY(recipe_id) 
                REFERENCES recipes(recipe_id),
-             FOREIGN KEY(meal_id) 
-               REFERENCES meals(meal_id)              
+            FOREIGN KEY(meal_id) 
+               REFERENCES meals(meal_id) 
+            );               
+            CREATE TABLE IF NOT EXISTS quantity (
+            quantity_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            measure_id INTEGER NOT NULL,
+            ingredient_id INTEGER NOT NULL,
+            quantity INTEGER NOT NULL,
+            recipe_id INTEGER NOT NULL,
+            FOREIGN KEY(measure_id) 
+               REFERENCES measures(measure_id),
+            FOREIGN KEY(ingredient_id) 
+               REFERENCES ingredients(ingredient_id),              
+            FOREIGN KEY(recipe_id) 
+               REFERENCES recipes(recipe_id)              
             );                       
             ''')
 
@@ -73,6 +86,14 @@ class RecipeDatabase:
                 VALUES (?, ?);
                 ''', (recipe_id, meal_id))
 
+    def add_quantity(self, value, measure_id, ingredient_id, recipe_id):
+        with sqlite3.connect(self.__database__) as data:
+            cursor = data.cursor()
+            cursor.execute('''
+            INSERT OR IGNORE INTO quantity (recipe_id, ingredient_id, measure_id, quantity)
+            VALUES (?, ?, ?, ?);
+            ''', (recipe_id, ingredient_id, measure_id, value))
+
     def get_meals(self):
         with sqlite3.connect(self.__database__) as data:
             cursor = data.cursor()
@@ -81,6 +102,45 @@ class RecipeDatabase:
             FROM meals;
             ''')
             return cursor.fetchall()
+
+    def get_ingredient(self, mask):
+        with sqlite3.connect(self.__database__) as data:
+            cursor = data.cursor()
+            cursor.execute(f'''
+            SELECT ingredient_id, ingredient_name
+            FROM ingredients
+            WHERE ingredient_name LIKE ?;
+            ''', (f'%{mask}%',))
+            result = cursor.fetchall()
+            if len(result) == 1:
+                return result[0][0]
+        return None
+
+    def get_measure(self, mask):
+        with sqlite3.connect(self.__database__) as data:
+            cursor = data.cursor()
+            cursor.execute(f'''
+            SELECT measure_id
+            FROM measures
+            WHERE measure_name LIKE ?;
+            ''', (f'{mask}%',))
+            result = cursor.fetchall()
+            if len(result) == 1:
+                return result[0][0]
+        return None
+
+    def get_empty_measure(self):
+        with sqlite3.connect(self.__database__) as data:
+            cursor = data.cursor()
+            cursor.execute(f'''
+            SELECT measure_id
+            FROM measures
+            WHERE measure_name LIKE ?;
+            ''', ('',))
+            result = cursor.fetchall()
+            if len(result) == 1:
+                return result[0][0]
+        return None
 
     def populate_book(self):
         print('Pass the empty recipe name to exit.')
@@ -94,9 +154,30 @@ class RecipeDatabase:
             meals_db = self.get_meals()
             for id_, meal in meals_db:
                 print(f'{id_}) {meal}  ', end='')
-            prompt_ = '\nWhen the dish can be served:'
+            prompt_ = '\nEnter proposed meals separated by a space:'
             result = list(map(int, input(prompt_).split()))
             self.add_serve(recipe_id, result)
+            ingredients_prompt = 'Input quantity of ingredient <press enter to stop>'
+            while True:
+                row_ = input(ingredients_prompt).split()
+                if not row_:
+                    break
+
+                if len(row_) == 2:
+                    value = int(row_[0])
+                    measure = self.get_empty_measure()
+                    ingredient = self.get_ingredient(row_[1])
+                else:
+                    value = int(row_[0])
+                    measure = self.get_measure(row_[1])
+                    ingredient = self.get_ingredient(row_[2])
+                if not measure:
+                    print('The measure is not conclusive!')
+                    continue
+                elif not ingredient:
+                    print('The ingredient is not conclusive!')
+                    continue
+                self.add_quantity(value, measure, ingredient, recipe_id)
 
 
 my_base = RecipeDatabase()
