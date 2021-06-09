ed8ad6e01d777a3742610802e6a8074f3ab5a500
Snetkova Natalia <snetkova.natalia@gmail.com>
2021-06-09 15:13:07 +0300

    Stage 5/6 Determined

--- Numeric Matrix Processor/processor.py
+++ Numeric Matrix Processor/processor.py
@@ -21,14 +21,27 @@ class Matrix:
     def addition(self):
         if self.nA == self.nB and self.mA == self.mB:
             self.mtrx_R = [[self.mtrx_A[i][j] + self.mtrx_B[i][j] for j in range(self.mA)] for i in range(self.nA)]
-            self.print_result()
+            self.print_matrix_result()
         else:
             print("The operation cannot be performed.")
 
     def const_mult(self):
         mult_const = int(input('Enter constant: '))
         self.mtrx_R = [[self.mtrx_A[i][j] * mult_const for j in range(self.mA)] for i in range(self.nA)]
-        self.print_result()
+        self.print_matrix_result()
+
+    def get_minor(self, matrix, i, j):
+        return [row[:j] + row[j + 1:] for row in matrix[:i] + matrix[i + 1:]]
+
+    def calculate_determinant(self, matrix):
+        det = 0
+        if len(matrix) == 1:
+            return matrix[0][0]
+        if len(matrix) == 2:
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+        for j, x in enumerate(matrix[0]):
+            det += matrix[0][j] * self.calculate_determinant(self.get_minor(matrix, 0, j)) * (-1) ** j
+        return det
 
     def transpose(self, param):
         if param == '1':
@@ -40,16 +53,16 @@ class Matrix:
         elif param == '4':
             self.mtrx_R = [[self.mtrx_A[i][j] for j in range(self.mA)] for i in range(self.nA)[::-1]]
  
-        self.print_result()
+        self.print_matrix_result()
 
     def mult(self):
         if self.mA == self.nB:
             self.mtrx_R = [[sum([self.mtrx_A[i][k]*self.mtrx_B[k][j] for k in range(self.nB)]) for j in range(self.mB)] for i in range(self.nA)]
-            self.print_result()
+            self.print_matrix_result()
         else:
             print("ERROR")
 
-    def print_result(self):
+    def print_matrix_result(self):
         print("The result is:")
         for row in self.mtrx_R:
             print(*row)
@@ -66,6 +79,7 @@ class Matrix:
                     "2. Multiply matrix by a constant",
                     "3. Multiply matrices",
                     "4. Transpose matrix",
+                    "5. Calculate a determinant",
                     "0. Exit"]
             print(*menu, sep='\n')
 
@@ -91,6 +105,10 @@ class Matrix:
                 diag_choice : str = input()
                 self.mtrx_A, self.nA, self.mA = self.read_mtrx()
                 self.transpose(diag_choice)
+            elif choice == '5':
+                self.mtrx_A, self.nA, self.mA = self.read_mtrx()
+                det = self.calculate_determinant(self.mtrx_A)
+                print("The result is:", det, sep='\n')
             elif choice == '0':
                 print('Bye!')
                 exit()
@@ -102,19 +120,24 @@ class Matrix:
 # line_2 = ['1 2 3 4 5','3 2 3 2 1','8 0 9 9 1','1 3 4 5 6']
 # line_3 = '4 5'
 # line_4 = ['1 1 4 4 5','4 4 5 7 8','1 2 3 9 8','1 0 0 0 1']
+# line_1 = '5 5'
+# line_2 = ['1 2 3 4 5','4 5 6 4 3','0 0 0 1 5','1 3 9 8 7','5 8 4 7 11']
 
-
-line_1 = '3 3'
-line_2 = ['1 7 7','6 6 4','4 2 1']
-line_3 = '3 3'
-line_4 = ['3 2 4','5 5 9','8 0 10']
-
+#
+# line_1 = '3 3'
+# line_2 = ['1 7 7','6 6 4','4 2 1']
+# line_3 = '3 3'
+# line_4 = ['3 2 4','5 5 9','8 0 10']
 
 # fabric = Matrix()
 # fabric.mtrx_A, fabric.nA, fabric.mA = fabric.read_mtrx(line_1, line_2)
 # # fabric.mtrx_B, fabric.nB, fabric.mB = fabric.read_mtrx(line_3, line_4)
 # # fabric.mult()
 # fabric.transpose('2')
+
+# fabric = Matrix()
+# fabric.mtrx_A, fabric.nA, fabric.mA = fabric.read_mtrx(line_1, line_2)
+# print(fabric.calculate_determinant(fabric.mtrx_A))
 
 fabric = Matrix()
 fabric.main()
