cf042355ea4253eee93be52ac3f180bfe6dc398a
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-02-29 16:13:57 +0300

    File Manager Stage 3/5 mkdir, rm, mv

--- File Manager/manager.py
+++ File Manager/manager.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 
 def cd_processing(_user_path, _argument):
@@ -44,6 +45,56 @@ while True:
     if user_command[0] == 'pwd':
         # print("Hi, pwd!")
         print(user_path)
+    elif user_command[0] == 'mkdir':
+        if len(user_command) == 1:
+            print('Specify the name of the directory to be made')
+            continue
+        try:
+            new_dir = user_command[1]
+            if not os.path.isabs(new_dir):
+                new_dir = user_path + '/' + new_dir
+
+            if os.path.isfile(new_dir) or os.path.isdir(new_dir):
+                print('The directory already exists')
+            else:
+                os.mkdir(new_dir)
+        except:
+            print('No such file or directory')
+    elif user_command[0] == 'mv':
+        if len(user_command) < 2:
+            print('Specify the current name of the file or directory and the new name')
+            continue
+        _argument = user_command[1].split(' ')
+        if len(_argument) < 2:
+            print('Specify the current name of the file or directory and the new name')
+            continue
+        old_path = user_path + '/' + _argument[0]
+        new_path = user_path + '/' + _argument[1]
+        if not os.path.isfile(old_path) and \
+            not os.path.isdir(old_path):
+            print('No such file or directory')
+            continue
+        if os.path.isfile(new_path) or \
+            os.path.isdir(new_path):
+            print('The file or directory already exists')
+            continue
+        shutil.move(old_path, new_path)
+    elif user_command[0] == 'rm':
+        if len(user_command) == 1:
+            print('Specify the file or directory')
+            continue
+        try:
+            rm_path = user_command[1]
+            if not os.path.isabs(user_command[1]):
+                rm_path = user_path + '/' + rm_path
+
+            if os.path.isfile(rm_path):
+                os.remove(rm_path)
+            else:
+                shutil.rmtree(rm_path)
+        except:
+            print('No such file or directory')
+
     elif user_command[0] == 'cd':
         try:
             _command, _argument = user_command[0], user_command[1]
