b29e32092c789918455dea8f4363921421b581e3
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-06-12 15:26:08 +0300

    Readability Score Stage 3/5

--- Readability Score (Python)/readability.py
+++ Readability Score (Python)/readability.py
@@ -1,6 +1,14 @@
 import argparse
+import math
 import nltk
 from nltk.tokenize import sent_tokenize
+
+def index_description(idx):
+    if idx >= 14:
+        return '18-22'
+    else:
+        return f'{idx + 4}-{idx + 5}'
+
 
 #  Download the necessary NLTK models for sentence tokenization
 nltk.download('punkt')
@@ -15,7 +23,10 @@ with open(args.file) as f:
     text = f.read()
 
 # Count the number of symbols in the text
-count_symbols = len(text)
+count_symbols = len(text
+                    .replace(' ', '')
+                    .replace('\n', '')
+                    .replace('\t', ''))
 
 # Count the number of word in the text
 count_words = len(nltk.regexp_tokenize(text, "[0-9A-z']+"))
@@ -23,12 +34,17 @@ count_words = len(nltk.regexp_tokenize(text, "[0-9A-z']+"))
 # Tokenize the text into sentences and count them
 count_sent = len(sent_tokenize(text))
 
-# Calculate the average number of words
-average_words = count_words / count_sent
+# Calculate Automated readability index
+score = 4.71 * count_symbols / count_words + 0.5 * count_words / count_sent - 21.43
+score = math.ceil(score)
 
 # Determine the difficulty of the text
-difficulty = 'HARD' if average_words > 10 else 'EASY'
+# difficulty = 'HARD' if average_words > 10 else 'EASY'
 
 # Output the result
-print(f'Text: {text}')
-print(f'Difficulty: {difficulty}')
+print(f'Text: {text}\n')
+print(f'Characters: {count_symbols}')
+print(f'Sentences: {count_sent}')
+print(f'Words: {count_words}')
+print(f'Automated Readability Index: {score}'
+      f' (this text should be understood by {index_description(score)} year olds).')
