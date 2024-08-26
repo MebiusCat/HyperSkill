aec25f798bbd027c9dcfd935f6819e036d8cd2a2
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-08-26 12:31:51 +0300

    Hospitals Stage 2/5 merge in one

--- Data Analysis for Hospitals/analysis.py
+++ Data Analysis for Hospitals/analysis.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+
+np.random.seed(30)
 
 if __name__ == '__main__':
     pd.set_option('display.max_columns', 8)
@@ -7,6 +10,12 @@ if __name__ == '__main__':
     df_prenatal = pd.read_csv('test/prenatal.csv')
     df_sport = pd.read_csv('test/sports.csv')
 
-    print(df_general.head(20))
-    print(df_prenatal.head(20))
-    print(df_sport.head(20))
+    df_prenatal.columns = df_general.columns
+    df_sport.columns = df_general.columns
+
+    df = pd.concat(
+        [df_general, df_prenatal, df_sport], ignore_index=True)
+
+    df.drop(columns=['Unnamed: 0'], inplace=True)
+    print(df.sample(20))
+
