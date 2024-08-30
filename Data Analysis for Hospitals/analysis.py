611f85f52480062bedef0c0e8d9f6ede1edd1c53
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-08-30 13:33:11 +0300

    Hospitals Stage 5/5 a lots of plots

--- Data Analysis for Hospitals/analysis.py
+++ Data Analysis for Hospitals/analysis.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -43,27 +44,27 @@ if __name__ == '__main__':
                     .agg({'gender': 'count'})
                     .sort_values('gender', ascending=False).iloc[0].name)
 
-    print(f'The answer to the 1st question is {max_patients}')
+    # print(f'The answer to the 1st question is {max_patients}')
 
     # 2. What share of the patients in the general hospital suffers from stomach-related issues?
     stomach_share = (df[(df.hospital == 'general') &
                         (df.diagnosis == 'stomach')].shape[0] /
                      df[df.hospital == 'general'].shape[0])
 
-    print(f'The answer to the 2nd question is {stomach_share:.3}')
+    # print(f'The answer to the 2nd question is {stomach_share:.3}')
 
     # 3. What share of the patients in the sports hospital suffers from dislocation-related issues?
     dis_share = (df[(df.hospital == 'sports') &
                    (df.diagnosis == 'dislocation')].shape[0] /
                  df[df.hospital == 'sports'].shape[0])
 
-    print(f'The answer to the 3rd question is {dis_share:.3}')
+    # print(f'The answer to the 3rd question is {dis_share:.3}')
 
     # 4. What is the difference in the median ages of the patients in the general and sports hospitals?
     age_differance = (df[df.hospital == 'general'].age.median() -
                       df[df.hospital == 'sports'].age.median())
 
-    print(f'The answer to the 4th question is {int(age_differance)}')
+    # print(f'The answer to the 4th question is {int(age_differance)}')
 
     # 5. How many blood tests were taken?
     max_test = (
@@ -73,4 +74,29 @@ if __name__ == '__main__':
                      aggfunc='count',
                      sort=True).iloc)[-1]
 
-    print(f'The answer to the 5th question is {max_test.name}, {int(max_test.blood_test)} blood tests')
+    # print(f'The answer to the 5th question is {max_test.name}, {int(max_test.blood_test)} blood tests')
+
+    # Stage 5
+
+    # What is the most common age of a patient among all hospitals?
+    df.age.plot(kind='hist', bins=[0, 15, 35, 55, 70])
+    plt.show()
+
+    # What is the most common diagnosis among patients in all hospitals?
+    df.diagnosis.value_counts().plot(kind='pie')
+    plt.show()
+
+    # Build a violin plot of height distribution by hospitals.
+    fig, axes = plt.subplots()
+    axes.set_title('Height distribution')
+    heights = [df[df.hospital == 'general'].height,
+               df[df.hospital == 'prenatal'].height,
+               df[df.hospital == 'sports'].height]
+    plt.violinplot(heights)
+    axes.set_xticks([1, 2, 3])
+    axes.set_xticklabels(['general', 'prenatal', 'sports'])
+    plt.show()
+
+    print('The answer to the 1st question: 15-35')
+    print('The answer to the 2nd question: pregnancy')
+    print('The answer to the 3rd question: different measuring system')
