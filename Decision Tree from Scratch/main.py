d53e225feb7a73c112493dfe52f8180cb6d06f28
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-09-04 13:08:42 +0300

    Decision Tree Stage 3/9 recursive split

--- Decision Tree from Scratch/main.py
+++ Decision Tree from Scratch/main.py
@@ -2,25 +2,72 @@
 import pandas as pd
 
 
-def gini(labels: list[str]) -> float:
+class Node:
+
+  def __init__(self):
+    # class initialization
+    self.left = None
+    self.right = None
+    self.term = False
+    self.label = None
+    self.feature = None
+    self.value = None
+
+  def set_split(self, feature, value):
+    # this function saves the node splitting feature and its value
+    self.feature = feature
+    self.value = value
+
+  def set_term(self, label):
+    # if the node is a leaf, this function saves its label
+    self.term = True
+    self.label = label
+
+
+def gini(labels) -> float:
     probabilities = [label_count / len(labels) for label_count in labels.value_counts()]
     return 1 - sum([prob ** 2 for prob in probabilities])
 
 
-def weighted_gini(left: list[str], right: list[str]) -> float:
+def weighted_gini(left, right) -> float:
     n = len(left) + len(right)
     return len(left) / n * gini(left) + len(right) / n * gini(right)
 
 
-def split(data, labels):
+def split(data: pd.DataFrame, labels: pd.Series):
     result = []
     for col in data.columns:
         for val in data[col].unique():
             aa = weighted_gini(labels[data[col] == val], labels[data[col] != val])
             result.append((aa, col, val))
     gini_value, col, val = min(result, key=lambda x: x[0])
-    print(round(gini_value, 4), col, val,
-          data[data[col] == val].index.to_list(), data[data[col] != val].index.to_list())
+    return (round(gini_value, 4), col, val,
+            data[data[col] == val].index.to_list(), data[data[col] != val].index.to_list())
+
+
+def split_data(node: Node, data: pd.DataFrame, labels: pd.Series) -> None:
+    if is_leaf(data, labels):
+        node.set_term(labels[0])
+    else:
+        _, feature, value, left, right = split(data, labels)
+        print(f'Made split: {feature} is {value}')
+        node.feature = feature
+        node.value = value
+        node.left = Node()
+        split_data(node.left, data.iloc[left].reset_index(drop=True),
+                   labels.iloc[left].reset_index(drop=True))
+        node.right = Node()
+        split_data(node.right, data.iloc[right].reset_index(drop=True),
+                   labels.iloc[right].reset_index(drop=True))
+
+
+def is_leaf(data, labels):
+    if len(data) == 1:
+        return True
+    if gini(labels) == 0:
+        return True
+    if max([data[col].nunique() for col in data.columns]) == 1:
+        return True
 
 
 def gini_test():
@@ -37,11 +84,14 @@ def gini_test():
 
 
 def main() -> None:
-    # file_path = '../data/data_stage2.csv'
+    # file_path = '../data/data_stage3.csv'
+
     file_path = input()
     df = pd.read_csv(file_path, index_col=0)
     X, y = df.iloc[:, :-1], df.iloc[:, -1]
-    split(X, y)
+
+    root = Node()
+    split_data(root, X, y)
 
 
 if __name__ == '__main__':
