02dd2567672628f1a0874d7e7d8e7059f00e0fd3
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-09-23 20:52:36 +0300

    Salary Prediction Stage 4/5 correlation

--- Salary Prediction/solution.py
+++ Salary Prediction/solution.py
@@ -2,6 +2,7 @@ import os
 import requests
 
 import pandas as pd
+from itertools import combinations
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_percentage_error as mape
@@ -45,17 +46,33 @@ def multi_model():
     # read data
     data = pd.read_csv('../Data/data.csv')
 
-    result = {}
-
     X, y = data.drop(['salary'], axis=1), data['salary']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
 
     model = LinearRegression()
     model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
 
     print(*model.coef_, sep=', ')
 
+
+def analysis():
+    # read data
+    data = pd.read_csv('../Data/data.csv')
+
+    result = {}
+    variations = ['rating', 'age', 'experience']
+
+    drops = variations + [[*el] for el in list(combinations(variations, 2))]
+    for i, col in enumerate(drops):
+        X, y = data.drop(col, axis=1).drop('salary', axis=1), data['salary']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
+
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        result[i] = (col, mape(y_test, y_pred))
+
+    print(f'{min(result.values(), key=lambda x: x[1])[1]:.5f}')
 
 # checking ../Data directory presence
 if not os.path.exists('../Data'):
@@ -67,4 +84,4 @@ if 'data.csv' not in os.listdir('../Data'):
     r = requests.get(url, allow_redirects=True)
     open('../Data/data.csv', 'wb').write(r.content)
 
-multi_model()
+analysis()
