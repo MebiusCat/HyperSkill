75c31338173a18d86509af8b16ae4ab975e78422
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-10-30 18:17:05 +0300

    Handwritten Digits Stage 5/5 hyperparameter tuning

--- Classification of Handwritten Digits/analysis.py
+++ Classification of Handwritten Digits/analysis.py
@@ -7,6 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import Normalizer
+from sklearn.model_selection import GridSearchCV
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -18,6 +19,29 @@ def fit_predict_eval(model, features_train, features_test, target_train, target_
     score = accuracy_score(target_test, y_pred)
     print(f'Model: {model}\nAccuracy: {score}\n')
     return round(score, 3)
+
+def base_models():
+    models = [KNeighborsClassifier(),
+              DecisionTreeClassifier(random_state=40),
+              LogisticRegression(),
+              RandomForestClassifier(random_state=40)
+              ]
+
+    acc_scores = []
+
+    for model in models:
+        acc_scores.append(
+            fit_predict_eval(
+                model=model,
+                features_train=X_train_norm,
+                features_test=X_test_norm,
+                target_train=y_train,
+                target_test=y_test
+            )
+        )
+
+    print('The answer to the 1st question: yes')
+    print('The answer to the 2nd question: KNeighborsClassifier-0.953, RandomForestClassifier-0.937')
 
 
 (X, y), (_, _) = keras.datasets.mnist.load_data(path='mnist.npz')
@@ -31,24 +55,34 @@ normalizer.fit(X_train, y_train)
 X_train_norm = normalizer.transform(X_train)
 X_test_norm = normalizer.transform(X_test)
 
-models = [KNeighborsClassifier(),
-          DecisionTreeClassifier(random_state=40),
-          LogisticRegression(),
-          RandomForestClassifier(random_state=40)
-          ]
+def eval(model, params):
+    grid_search = GridSearchCV(model, param_grid=params, n_jobs=-1)
+    grid_search.fit(X_train_norm, y_train)
 
-acc_scores = []
+    # print(name)
+    # print(f'best estimator: {grid_search.best_estimator_}')
+    # print(grid_search.best_params_)
+    # print(f'accuracy: {grid_search.best_score_}')
+    return grid_search.best_estimator_
 
-for model in models:
-    acc_scores.append(
-        fit_predict_eval(
-            model=model,
-            features_train=X_train_norm,
-            features_test=X_test_norm,
-            target_train=y_train,
-            target_test=y_test
-        )
-    )
+kn_params = {
+    'n_neighbors': [3, 4],
+    'weights': ['uniform', 'distance'],
+    'algorithm': ['auto', 'brute'],
+    'p': [1, 2],
+}
 
-print('The answer to the 1st question: yes')
-print('The answer to the 2nd question: KNeighborsClassifier-0.953, RandomForestClassifier-0.937')
+rf_params = {
+    'n_estimators': [300, 500, 600],
+    'max_features': ['sqrt', 'log2'],
+    'class_weight': ['balanced', 'balanced_subsample']
+}
+knn = eval(KNeighborsClassifier(), kn_params)
+print('K-nearest neighbours algorithm')
+print(f'best estimator: {knn}')
+print(f'accuracy: {accuracy_score(y_test, knn.predict(X_test_norm))}')
+
+rf = eval(RandomForestClassifier(random_state=40), rf_params)
+print('Random forest algorithm')
+print(f'best estimator: {rf}')
+print(f'accuracy: {accuracy_score(y_test, rf.predict(X_test_norm))}')
