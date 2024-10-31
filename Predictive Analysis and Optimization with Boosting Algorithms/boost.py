b7654d763e4cef617d17c5b8140af4bad5d7fde1
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-10-31 23:23:55 +0300

    Analysis Stage 4/5 boosting algorithms

--- Predictive Analysis and Optimization with Boosting Algorithms/boost.py
+++ Predictive Analysis and Optimization with Boosting Algorithms/boost.py
@@ -5,6 +5,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import mean_absolute_error
+
+from xgboost import XGBRegressor
+from lightgbm import early_stopping, LGBMRegressor
+from catboost import CatBoostRegressor
 
 def download_data():
     # Create data directory
@@ -46,9 +51,58 @@ X_train_norm = ct.transform(X_train)
 X_test_norm = ct.transform(X_test)
 X_val_norm = ct.transform(X_val)
 
-answer = {
-    'train': list(X_train_norm.shape),
-    'validation': list(X_val_norm.shape),
-    'test': list(X_test_norm.shape)
+n_jobs = -1
+learning_rate = 1e-1
+max_depth = 10
+n_estimators = 100
+eval_set = [(X_val_norm, y_val)]
+stopping_rounds = 5
+callbacks = [
+    early_stopping(stopping_rounds=stopping_rounds, verbose=False)
+]
+
+xgb = XGBRegressor(
+    objective='reg:squarederror', n_estiamtors=n_estimators,
+    learning_rate=learning_rate, max_depth=max_depth,
+    early_stopping=stopping_rounds, verbosity=0, n_jobs=n_jobs
+)
+
+lgbm = LGBMRegressor(
+    objective='regression', n_estimators=n_estimators,
+    learning_rate=learning_rate, max_depth=max_depth,
+    verbosity=-1, n_jobs=n_jobs
+)
+
+catboost = CatBoostRegressor(
+    loss_function='RMSE', iterations=n_estimators,
+    learning_rate=learning_rate, max_depth=max_depth,
+    early_stopping_rounds=stopping_rounds, silent=True, thread_count=n_jobs
+)
+
+# for XGBoost and CatBoost
+xgb.fit(X_train_norm, y_train, eval_set=eval_set, verbose=False)
+catboost.fit(X_train_norm, y_train, eval_set=eval_set, verbose=False)
+
+# for LightGBM
+lgbm.fit(X_train_norm, y_train, eval_set=eval_set, callbacks=callbacks)
+
+result = {
+    'xgb_reg': [
+        mean_absolute_error(y_train, xgb.predict(X_train_norm)),
+        mean_absolute_error(y_val, xgb.predict(X_val_norm)),
+        mean_absolute_error(y_test, xgb.predict(X_test_norm))
+    ],
+    'cat_reg': [
+        mean_absolute_error(y_train, catboost.predict(X_train_norm)),
+        mean_absolute_error(y_val, catboost.predict(X_val_norm)),
+        mean_absolute_error(y_test, catboost.predict(X_test_norm))
+    ],
+    'lgbm_reg': [
+        mean_absolute_error(y_train, lgbm.predict(X_train_norm)),
+        mean_absolute_error(y_val, lgbm.predict(X_val_norm)),
+        mean_absolute_error(y_test, lgbm.predict(X_test_norm))
+    ]
 }
-print(answer)
+
+mae_df = pd.DataFrame(result, index=['mae_train', 'mae_val', 'mae_test'])
+mae_df.to_csv('../data/baseline.csv')
