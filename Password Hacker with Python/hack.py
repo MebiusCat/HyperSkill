86fa9168b3915fb7c3a55e18ff71a6bd267a39c5
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-11-20 19:06:58 +0300

    Password Hacker with Python Stage 2/5 brute force

--- Password Hacker with Python/hack.py
+++ Password Hacker with Python/hack.py
@@ -1,16 +1,31 @@
 import argparse
 import socket
 
+from itertools import combinations_with_replacement
+from string import ascii_lowercase, digits
+
+
+def guess_password():
+    pool = ascii_lowercase + digits
+    for size in range(1, 10):
+        for comb in combinations_with_replacement(pool, size):
+            yield ''.join(comb)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("hostname", type=str)
 parser.add_argument("port", type=int)
-parser.add_argument("password", type=str)
 args = parser.parse_args()
 
 with socket.socket() as client_socket:
     address = (args.hostname, args.port)
 
     client_socket.connect(address)
-    client_socket.send(args.password.encode())
-    print(client_socket.recv(1024).decode())
+    gen = guess_password()
+    while True:
+        cur_password = next(gen)
+        client_socket.send(cur_password.encode())
+        message = client_socket.recv(1024).decode()
+        if message == 'Connection success!':
+            print(cur_password)
+            break
