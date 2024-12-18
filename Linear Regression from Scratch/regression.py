a1e6ef529d0074d9c429a7e0d52ab3258cbd6b1f
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-12-18 15:31:44 +0300

    Linear Regression Stage 3/4 metrics

--- Linear Regression from Scratch/regression.py
+++ Linear Regression from Scratch/regression.py
@@ -21,20 +21,24 @@ class CustomLinearRegression:
 
     def predict(self, X):
         if self.fit_intercept:
-            return X @ np.column_stack(self.coefficient, self.intercept)
+            return (np.column_stack((X, np.ones(len(X))))
+                    @ np.hstack((self.coefficient, self.intercept)))
         else:
             return X @ self.coefficient
 
     def r2_score(self, y, yhat):
-        pass
+        return 1. - np.sum((y - yhat) ** 2) / np.sum((y - np.mean(y)) ** 2)
 
     def rmse(self, y, yhat):
-        pass
+        return np.sqrt(1. / len(y) * np.sum((y - yhat) ** 2))
 
 
-    def get_weights(self):
+    def get_weights(self, y, yhat):
         return {'Intercept': self.intercept,
-                'Coefficient': self.coefficient}
+                'Coefficient': self.coefficient,
+                'R2': float(self.r2_score(y, yhat)),
+                'RMSE': float(self.rmse(y, yhat)),
+                }
 
 
 def main():
@@ -43,15 +47,21 @@ def main():
     # y = [33, 42, 45, 51, 53, 61, 62]
 
     # Stage 2
-    x = [4, 4.5, 5, 5.5, 6, 6.5, 7]
-    w = [1, -3, 2, 5, 0, 3, 6]
-    z = [11, 15, 12, 9, 18, 13, 16]
-    y = [33, 42, 45, 51, 53, 61, 62]
-    X = np.column_stack([x, w, z])
-    model = CustomLinearRegression(fit_intercept=False)
+    # x = [4, 4.5, 5, 5.5, 6, 6.5, 7]
+    # w = [1, -3, 2, 5, 0, 3, 6]
+    # z = [11, 15, 12, 9, 18, 13, 16]
+    # y = [33, 42, 45, 51, 53, 61, 62]
+
+    # Stage 3
+    x = [0.9, 0.5, 1.75, 2.0, 1.4, 1.5, 3.0, 1.1, 2.6, 1.9]
+    w = [11, 11, 9, 8, 7, 7, 6, 5, 5, 4]
+    y = [21.95, 27.18, 16.9, 15.37, 16.03, 18.15, 14.22, 18.72, 15.4, 14.69]
+
+    X = np.column_stack([x, w])
+    model = CustomLinearRegression()
     model.fit(X, y)
 
-    print(model.predict(X))
+    print(model.get_weights(y, model.predict(X)))
 
 
 if __name__ == '__main__':
