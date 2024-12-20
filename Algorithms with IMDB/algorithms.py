0db472bbaf266afc45450566abb63010cc37c46b
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-12-20 19:23:13 +0300

    Algorithms Stage 3/5 bubble sort

--- Algorithms with IMDB/algorithms.py
+++ Algorithms with IMDB/algorithms.py
@@ -1,13 +1,20 @@
 import csv
 
-PWD = '/Users/mebiuscat/Downloads/movies.csv'
-
 with open('../data/movies.csv', newline='', encoding="UTF-8") as data:
     file_reader = csv.reader(data)
 
-    moviedb = {}
+    moviedb = []
     for line in file_reader:
         movie, rating = line
-        moviedb[movie] = float(rating)
-        if float(rating) == 6.0:
-            print(f'{line[0]} - {line[1]}')
+        moviedb.append((movie, float(rating)))
+
+
+for i in range(len(moviedb) - 1):
+    offset = 0
+    for j in range(offset + 1, len(moviedb)):
+        if moviedb[offset][1] > moviedb[j][1]:
+            moviedb[offset], moviedb[j] = moviedb[j], moviedb[offset]
+        offset = j
+
+for movie, rating in moviedb:
+    print(f'{movie} - {rating}')
