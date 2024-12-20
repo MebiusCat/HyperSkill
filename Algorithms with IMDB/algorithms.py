f35fc9ea6b8d57423fb7dc7dd395820d9f805733
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-12-20 18:00:50 +0300

    IMDB Stage 2/5 filter

--- Algorithms with IMDB/algorithms.py
+++ Algorithms with IMDB/algorithms.py
@@ -1,10 +1,13 @@
 import csv
 
+PWD = '/Users/mebiuscat/Downloads/movies.csv'
+
 with open('../data/movies.csv', newline='', encoding="UTF-8") as data:
     file_reader = csv.reader(data)
 
     moviedb = {}
     for line in file_reader:
-        moviedb[line[0]] = line[1]
-        print(f'{line[0]} - {line[1]}')
-
+        movie, rating = line
+        moviedb[movie] = float(rating)
+        if float(rating) == 6.0:
+            print(f'{line[0]} - {line[1]}')
