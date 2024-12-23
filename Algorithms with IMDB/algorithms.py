9f51a048c3f204a3974c8cc53b4d917a7018ca70
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-12-23 19:01:05 +0300

    IMDB Stage 4/5 binary search

--- Algorithms with IMDB/algorithms.py
+++ Algorithms with IMDB/algorithms.py
@@ -1,5 +1,30 @@
 import csv
 
+def bubble_sort(moviedb):
+    for i in range(len(moviedb) - 1):
+        offset = 0
+        for j in range(offset + 1, len(moviedb)):
+            if moviedb[offset][1] > moviedb[j][1]:
+                moviedb[offset], moviedb[j] = moviedb[j], moviedb[offset]
+            offset = j
+
+
+def binary_search(moviedb, rating):
+    low, high = 0, len(moviedb) - 1
+    while low < high:
+        mid = int((high + low) // 2)
+        mrating = moviedb[mid][1]
+        if mrating >= rating:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    while low < len(moviedb) and moviedb[low][1] == rating:
+        movie, rating = moviedb[low]
+        print(f'{movie} - {rating}')
+        low += 1
+
+
 with open('../data/movies.csv', newline='', encoding="UTF-8") as data:
     file_reader = csv.reader(data)
 
@@ -8,13 +33,12 @@ with open('../data/movies.csv', newline='', encoding="UTF-8") as data:
         movie, rating = line
         moviedb.append((movie, float(rating)))
 
+    bubble_sort(moviedb)
+    binary_search(moviedb, 6.0)
 
-for i in range(len(moviedb) - 1):
-    offset = 0
-    for j in range(offset + 1, len(moviedb)):
-        if moviedb[offset][1] > moviedb[j][1]:
-            moviedb[offset], moviedb[j] = moviedb[j], moviedb[offset]
-        offset = j
 
-for movie, rating in moviedb:
-    print(f'{movie} - {rating}')
+
+
+
+# for movie, rating in moviedb:
+#     print(f'{movie} - {rating}')
