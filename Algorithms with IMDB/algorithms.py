54f278f8fa4eaf81f06bb0f02b5c475b3be5c411
Natalia <57619785+MebiusCat@users.noreply.github.com>
2024-12-24 13:51:03 +0300

    IMDB Stage 5/5 merge sort

--- Algorithms with IMDB/algorithms.py
+++ Algorithms with IMDB/algorithms.py
@@ -25,6 +25,33 @@ def binary_search(moviedb, rating):
         low += 1
 
 
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i][1] < right[j][1]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
 with open('../data/movies.csv', newline='', encoding="UTF-8") as data:
     file_reader = csv.reader(data)
 
@@ -33,12 +60,6 @@ with open('../data/movies.csv', newline='', encoding="UTF-8") as data:
         movie, rating = line
         moviedb.append((movie, float(rating)))
 
-    bubble_sort(moviedb)
+    # bubble_sort(moviedb)
+    moviedb = merge_sort(moviedb)
     binary_search(moviedb, 6.0)
-
-
-
-
-
-# for movie, rating in moviedb:
-#     print(f'{movie} - {rating}')
